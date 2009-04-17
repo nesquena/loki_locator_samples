3baec1053227571b2deeaf55c0648ef48a213944
Nathan Esquenazi <nesquena@gmail.com>
2009-04-17 05:23:06 -0700

    Initial commit of basic loki sample which prints current address

---
+++ loki.js
@@ -0,0 +1,11 @@
+document.observe('dom:loaded', function(){
+	  var loki = new LokiAPI();
+	  loki.onSuccess = function(location) {
+	    $('address').update(location.house_number+' '+location.street+'<br/>'+location.city+', '+location.region_code+' '+location.postal_code+'<br/>'+location.latitude+', '+location.longitude);
+	  };
+	  loki.onFailure = function(error, msg){
+	    alert('An error has been encountered ('+error+'). '+msg);
+	  };
+	  loki.setKey('beta');
+	  loki.requestLocation(true,loki.FULL_STREET_ADDRESS_LOOKUP);
+});
